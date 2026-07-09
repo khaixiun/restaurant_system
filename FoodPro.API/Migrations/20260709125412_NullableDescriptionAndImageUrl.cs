cf2e2bf7e5d5ba74cb3b177429a001bd91a1719a
khaixiun <khaixiun123@gmail.com>
2026-07-09 20:57:22 +0800

    Migrations file

---
+++ FoodPro.API/Migrations/20260709125412_NullableDescriptionAndImageUrl.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FoodPro.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class NullableDescriptionAndImageUrl : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "image_url",
+                table: "foods",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "description",
+                table: "foods",
+                type: "varchar(500)",
+                maxLength: 500,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(500)",
+                oldMaxLength: 500)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "foods",
+                keyColumn: "image_url",
+                keyValue: null,
+                column: "image_url",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "image_url",
+                table: "foods",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.UpdateData(
+                table: "foods",
+                keyColumn: "description",
+                keyValue: null,
+                column: "description",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "description",
+                table: "foods",
+                type: "varchar(500)",
+                maxLength: 500,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(500)",
+                oldMaxLength: 500,
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}

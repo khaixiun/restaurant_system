6a42e0f5332819dbe5371ec8fc39895c2f6e58f0
khaixiun <khaixiun123@gmail.com>
2026-07-10 13:55:07 +0800

    Added for swagger file testing

--- FoodPro.API/Program.cs
+++ FoodPro.API/Program.cs
@@ -1,5 +1,6 @@
 using FoodPro.API.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,13 +10,25 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
 
 builder.Services.AddControllers();
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.Limits.MaxRequestBodySize = 5 * 1024 * 1024;
+});
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.MapType<IFormFile>(() => new OpenApiSchema 
+    {
+        Type = JsonSchemaType.String,
+        Format = "binary"
+    });
+});
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage();
     app.UseSwagger();
     app.UseSwaggerUI();
 }
